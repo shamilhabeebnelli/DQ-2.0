3442cf86130e21a8bbc2273fe062c49e73683244
mrdayamzaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-11-30 22:00:43 +0530

    Add files via upload

---
+++ WhiteEyeUserBot/modules/imagetools.py
@@ -0,0 +1,160 @@
+# Api Provided By @PythonvsAno
+#    Copyright (C) Midhun KM 2020
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+
+import cv2
+import requests
+from PIL import Image
+from telethon.tl.types import MessageMediaPhoto
+
+from WhiteEyeUserBot.utils import WhiteEye_on_cmd, sudo_cmd
+
+sedpath = "./starkgangz/"
+if not os.path.isdir(sedpath):
+    os.makedirs(sedpath)
+
+
+@WhiteEye.on(WhiteEye_on_cmd(pattern=r"cit"))
+@WhiteEye.on(sudo_cmd(pattern=r"cit", allow_sudo=True))
+async def hmm(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    headers = {"api-key": life}
+    hmm = await event.edit("Colourzing..")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in response.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    img_file = {
+        "image": open(img, "rb"),
+    }
+    url = "https://api.deepai.org/api/colorizer"
+    r = requests.post(url=url, files=img_file, headers=headers).json()
+    sedimg = r["output_url"]
+    await borg.send_file(event.chat_id, sedimg)
+    await hmm.delete()
+    if os.path.exists(img):
+        os.remove(img)
+
+
+@WhiteEye.on(WhiteEye_on_cmd(pattern=r"toon"))
+@WhiteEye.on(sudo_cmd(pattern=r"toon", allow_sudo=True))
+async def hmm(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    headers = {"api-key": life}
+    hmm = await event.edit("Toooning.....")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in response.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    img_file = {
+        "image": open(img, "rb"),
+    }
+    url = "https://api.deepai.org/api/toonify"
+    r = requests.post(url=url, files=img_file, headers=headers).json()
+    sedimg = r["output_url"]
+    await borg.send_file(event.chat_id, sedimg)
+    await hmm.delete()
+    if os.path.exists(img):
+        os.remove(img)
+
+
+@WhiteEye.on(WhiteEye_on_cmd(pattern=r"nst"))
+@WhiteEye.on(sudo_cmd(pattern=r"nst", allow_sudo=True))
+async def hmm(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    headers = {"api-key": life}
+    hmm = await event.edit("Colourzing..")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in response.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    img_file = {
+        "image": open(img, "rb"),
+    }
+    url = "https://api.deepai.org/api/nsfw-detector"
+    r = requests.post(url=url, files=img_file, headers=headers).json()
+    sedcopy = r["output"]
+    hmmyes = sedcopy["detections"]
+    game = sedcopy["nsfw_score"]
+    final = f"**IMG RESULT** \n**Detections :** `{hmmyes}` \n**NSFW SCORE :** `{game}`"
+    await borg.send_message(event.chat_id, final)
+    await hmm.delete()
+    if os.path.exists(img):
+        os.remove(img)
+
+
+@WhiteEye.on(WhiteEye_on_cmd(pattern=r"thug"))
+@WhiteEye.on(sudo_cmd(pattern=r"thug", allow_sudo=True))
+async def iamthug(event):
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    hmm = await event.edit("`Converting To thug Image..`")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in sed.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    imagePath = img
+    maskPath = "./resources/thuglife/mask.png"
+    cascPath = "./resources/thuglife/face_regex.xml"
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.15)
+    background = Image.open(imagePath)
+    for (x, y, w, h) in faces:
+        mask = Image.open(maskPath)
+        mask = mask.resize((w, h), Image.ANTIALIAS)
+        offset = (x, y)
+        background.paste(mask, offset, mask=mask)
+    file_name = "WhiteEyethug.png"
+    ok = sedpath + "/" + file_name
+    background.save(ok, "PNG")
+    await borg.send_file(event.chat_id, ok)
+    await hmm.delete()
