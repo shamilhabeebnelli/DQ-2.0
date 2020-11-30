fcbdf4d70e0027636f1ff06119b68ef82b00d333
mrdayamzaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-11-30 22:04:38 +0530

    Add files via upload

---
+++ WhiteEyeUserBot/modules/test.py
@@ -0,0 +1,17 @@
+from WhiteEyeUserBot.utils import WhiteEye_on_cmd
+from WhiteEyeUserBot import CMD_HELP
+
+
+@WhiteEye.on(WhiteEye_on_cmd(pattern=r"test"))
+async def test(event):
+    if event.fwd_from:
+        return
+    await event.edit("Test Successfull. Boss !")
+
+CMD_HELP.update(
+    {
+        "test": "Test\
+\n\nSyntax : .test\
+\nUsage : Another Fun Plugin"
+    }
+)    
