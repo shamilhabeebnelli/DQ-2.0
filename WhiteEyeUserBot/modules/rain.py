ca35cfbfb102b6b70286a53b1f0622eaf7cbd0ca
mrdayamzaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-11-30 22:02:47 +0530

    Add files via upload

---
+++ WhiteEyeUserBot/modules/rain.py
@@ -0,0 +1,24 @@
+# Originally created By KingMars ✅ Rain Sequence 2 {Updated}
+import asyncio
+from collections import deque
+from WhiteEyeUserBot import CMD_HELP
+from telethon import events
+
+
+@WhiteEye.on(events.NewMessage(pattern=r"\.km_rain2", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    deq = deque(list("☁️⛈Ř/~\İŇ🌬⚡🌪"))
+    for _ in range(100):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+        
+CMD_HELP.update(
+    {
+        "rain": "Rain\
+\n\nSyntax : .rain\
+\nUsage : Baarish"
+    }
+)        
