adb80ea717b30a695a92a5c7846398137017475c
mrdayamzaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-12-01 09:57:50 +0530

    Update Configs.py

--- WhiteEyeUserBot/Configs.py
+++ WhiteEyeUserBot/Configs.py
@@ -164,7 +164,7 @@ if ENV:
             "UPSTREAM_REPO", "https://github.com/WhiteEye-Org/WhiteEyeUserBot"
         )
         ALIVE_IMAGE = os.environ.get(
-            "ALIVE_IMAGE", "https://telegra.ph/file/63a02b3ef3245b321e54a.jpg"
+            "ALIVE_IMAGE", "https://telegra.ph/file/63d2f8bcdae4da2ec5e7e.jpg"
         )
         TESSDATA_PREFIX = os.environ.get(
             "TESSDATA_PREFIX", "./.apt/usr/share/tesseract-ocr/4.00/tessdata"
