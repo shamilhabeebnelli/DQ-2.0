7e6741579f69b098e6be3af955ef37bd95d0286d
mrdayamzaidi <dayamali975@gmail.com>
2020-12-05 05:58:55 +0000

    pylint: auto fixes

--- WhiteEyeUserBot/modules/antiflood.py
+++ WhiteEyeUserBot/modules/antiflood.py
@@ -4,8 +4,8 @@ from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights
 
 from WhiteEyeUserBot import CMD_HELP
-from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 from WhiteEyeUserBot.modules.sql_helper.antiflood_sql import antiflood_spl as sql
+from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 
 CHAT_FLOOD = sql.__load_flood_settings()
 # warn mode for anti flood
