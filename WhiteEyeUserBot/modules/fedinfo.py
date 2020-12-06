f9dcd7713fa37421c81ecd4e9409959ed91d7415
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-12-06 20:56:51 +0530

    Update fedinfo.py

--- WhiteEyeUserBot/modules/fedinfo.py
+++ WhiteEyeUserBot/modules/fedinfo.py
@@ -1,5 +1,5 @@
 #    TeleBot - UserBot
-#    Copyright (C) 2020 WhiteEye
+#    Copyright (C) 2020 TeleBot
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
