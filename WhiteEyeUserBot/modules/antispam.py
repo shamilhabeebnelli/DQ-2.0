50970ac769b85f16059d89a97a03d67ae99b81ba
mrdayamzaidi <dayamali975@gmail.com>
2020-12-06 14:53:30 +0000

    pylint: auto fixes

--- WhiteEyeUserBot/modules/antispam.py
+++ WhiteEyeUserBot/modules/antispam.py
@@ -1,7 +1,7 @@
 from telethon.events import ChatAction
-from WhiteEyeUserBot.Configs import Config
 
 from WhiteEyeUserBot import sclient
+from WhiteEyeUserBot.Configs import Config
 
 """Bans Spammers/Scammer At time Of Arrival 
 If You Add Him The Bot Won't Restrict."""
