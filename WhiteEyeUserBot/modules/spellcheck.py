fb7cca7830a228c02f5fe6bba9202fde50e57cac
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-12-09 11:13:06 +0530

    Create spellcheck.py

---
+++ WhiteEyeUserBot/modules/spellcheck.py
@@ -0,0 +1,30 @@
+from textblob import TextBlob
+
+from WhiteEyeUserBot import CMD_HELP
+from WhiteEyeUserBot.utils import WhiteEye_on_cmd
+
+
+@WhiteEye.on(WhiteEye_on_cmd(pattern="spellcheck (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    a = input_str
+
+    # print("original text: "+str(a))
+    b = TextBlob(a)
+    # print("corrected text: "+str(b.correct()))
+    c = b.correct()
+    await event.edit(
+        f"<b><u>Check Completed</b></u> \n\n<b>Original Text</b>:-  <code>{a}</code> \n<b>Corrected Text:-</b> <code>{c}</code>",
+        parse_mode="HTML",
+    )
+
+
+CMD_HELP.update(
+    {
+        "spellcheck": "**Spell Checker**\
+\n\n**Syntax : **`.spellcheck <text to check>`\
+\n**Usage :** Checks for spelling mistakes in given text."
+    }
+)
