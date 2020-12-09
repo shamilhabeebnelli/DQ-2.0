b9188726093bfe9e362145047b899efeb82ff48c
mrdayamzaidi <dayamali975@gmail.com>
2020-12-09 09:19:52 +0000

    pylint: auto fixes

--- WhiteEyeUserBot/modules/_inlinebot.py
+++ WhiteEyeUserBot/modules/_inlinebot.py
@@ -1,17 +1,13 @@
+import asyncio
+import os
 import re
 from math import ceil
-import asyncio
-import html
 
-from telethon import Button
-from telethon import custom
-from telethon import events
-from telethon import functions
-from telethon.tl.functions.users import GetFullUserRequest
-import os
-from WhiteEyeUserBot import ALIVE_NAME
-from WhiteEyeUserBot import CMD_LIST, CMD_HELP
+from telethon import Button, custom, events, functions
+
+from WhiteEyeUserBot import ALIVE_NAME, CMD_HELP, CMD_LIST
 from WhiteEyeUserBot.modules import inlinestats
+
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
     WARN_PIC = "https://telegra.ph/file/63d2f8bcdae4da2ec5e7e.jpg"
@@ -20,7 +16,7 @@ else:
 LOG_CHAT = Config.PRIVATE_GROUP_ID
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "WhiteEye"
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
-    
+
     @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
     async def inline_handler(event):
         builder = event.builder
@@ -31,8 +27,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
                 "© WhiteEye Help",
-                text="{}\nCurrently Loaded Plugins: {}".format(
-                    query, len(CMD_LIST)),
+                text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),
                 buttons=buttons,
                 link_preview=False,
             )
@@ -44,8 +39,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     [custom.Button.inline("Show Stats ", data="terminator")],
                     [
                         Button.url(
-                            "Repo 🇮🇳",
-                            "https://github.com/WhiteEye-Org/WhiteEyeUserBot")
+                            "Repo 🇮🇳", "https://github.com/WhiteEye-Org/WhiteEyeUserBot"
+                        )
                     ],
                     [Button.url("Join Channel ⚓", "t.me/WhiteEyeOT")],
                 ],
@@ -56,32 +51,32 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 text=query,
                 buttons=[
                     [
-                        custom.Button.inline("❌ Spamming",data="wannaspam"),
-                        custom.Button.inline("📝 Chatting",data="casualbitching")
+                        custom.Button.inline("❌ Spamming", data="wannaspam"),
+                        custom.Button.inline("📝 Chatting", data="casualbitching"),
                     ],
                     [
-                        custom.Button.inline("❓ Doubt",data="askme"),
-                        custom.Button.inline("🛑 Others",data="others")
-                    ]
-                ]
+                        custom.Button.inline("❓ Doubt", data="askme"),
+                        custom.Button.inline("🛑 Others", data="others"),
+                    ],
+                ],
             )
         await event.answer([result] if result else None)
 
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-            data=re.compile(b"helpme_next\((.+?)\)")))
+            data=re.compile(b"helpme_next\((.+?)\)")
+        )
+    )
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:  # pylint:disable=E0602
-            current_page_number = int(
-                event.data_match.group(1).decode("UTF-8"))
-            buttons = paginate_help(current_page_number + 1, CMD_LIST,
-                                    "helpme")
+            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+            buttons = paginate_help(current_page_number + 1, CMD_LIST, "helpme")
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
             reply_popp_up_alert = "Please get your own Userbot, and don't use mine!"
             await event.answer(reply_popp_up_alert, cache_time=0, alert=True)
-              
+
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:
@@ -92,26 +87,25 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             reply_pop_up_alert = "Please get your own userbot from @BlackPearlsupport "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-               
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-            data=re.compile(b"helpme_prev\((.+?)\)")))
+            data=re.compile(b"helpme_prev\((.+?)\)")
+        )
+    )
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:  # pylint:disable=E0602
-            current_page_number = int(
-                event.data_match.group(1).decode("UTF-8"))
+            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
             buttons = paginate_help(
-                current_page_number - 1,
-                CMD_LIST,
-                "helpme"  # pylint:disable=E0602
+                current_page_number - 1, CMD_LIST, "helpme"  # pylint:disable=E0602
             )
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own WhiteEyeUserbot, and don't use mine!"
+            reply_pop_up_alert = (
+                "Please get your own WhiteEyeUserbot, and don't use mine!"
+            )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-            
-            
+
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
             data=re.compile(b"us_plugin_(.*)")
@@ -156,9 +150,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = "You Cannot Become Barbosa Here!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-            
-    @tgbot.on(
-        events.callbackquery.CallbackQuery(data=re.compile(b"terminator")))
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"terminator")))
     async def rip(event):
         if event.query.user_id == bot.uid:
             text = inlinestats
@@ -167,10 +160,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             txt = "You Can't View My Masters Stats"
             await event.answer(txt, alert=True)
 
-    @tgbot.on(
-        events.callbackquery.CallbackQuery(data=re.compile(b"wannaspam")))
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"wannaspam")))
     async def rip(event):
-        chat_k = await event.get_chat()
+        await event.get_chat()
         text1 = "You Have Chosed A Probhited Option. Therefore, You Have Been Blocked By WhiteEye. 💢"
         await event.edit("Choice Not Accepted ❌")
         await borg.send_message(event.query.user_id, text1)
@@ -178,34 +170,31 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"casualbitching")))
     async def rip(event):
-        chat_m = await event.get_chat()
+        await event.get_chat()
         him_id = event.query.user_id
         await event.edit("Choice Accepted ✔️")
-        text2 = (
-            "Ok. Please Wait Until My Master Approves. Don't Spam Or Try Anything Stupid. \nThank You For Contacting Me."
-        )
+        text2 = "Ok. Please Wait Until My Master Approves. Don't Spam Or Try Anything Stupid. \nThank You For Contacting Me."
         await borg.send_message(event.query.user_id, text2)
-        await tgbot.send_message(LOG_CHAT, message=f"Hello, A [New User](tg://user?id={him_id}). Wants To Talk With You.",
-                                buttons=[
-                                Button.url(
-                                "Contact Him",
-                                f"tg://user?id={him_id}")
-                                ]
-                                )
+        await tgbot.send_message(
+            LOG_CHAT,
+            message=f"Hello, A [New User](tg://user?id={him_id}). Wants To Talk With You.",
+            buttons=[Button.url("Contact Him", f"tg://user?id={him_id}")],
+        )
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"askme")))
     async def rip(event):
-        chat_s = await event.get_chat()
+        await event.get_chat()
         await event.edit("Choice Accepted ✔️")
         text3 = "Ok, Wait. You can Ask After Master Approves You. Kindly, Wait."
         await borg.send_message(event.query.user_id, text3)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"others")))
     async def rip(event):
-        chat_s = await event.get_chat()
+        await event.get_chat()
         await event.edit("Choice Accepted ✔️")
         text4 = "Ok, Wait. You can Ask After Master Approves You. Kindly, Wait."
         await borg.send_message(event.query.user_id, text4)
+
 
 def paginate_help(page_number, loaded_modules, prefix):
     number_of_rows = 10
@@ -216,24 +205,28 @@ def paginate_help(page_number, loaded_modules, prefix):
             helpable_modules.append(p)
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} {}".format("⚓", x, "⚓"),
-                             data="us_plugin_{}".format(x))
+        custom.Button.inline(
+            "{} {} {}".format("⚓", x, "⚓"), data="us_plugin_{}".format(x)
+        )
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
     if len(modules) % number_of_cols == 1:
-        pairs.append((modules[-1], ))
+        pairs.append((modules[-1],))
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
-        pairs = pairs[modulo_page * number_of_rows:number_of_rows *
-                      (modulo_page + 1)] + [(
-                          custom.Button.inline("Previous",
-                                               data="{}_prev({})".format(
-                                                   prefix, modulo_page)),
-                          custom.Button.inline("❌Close", data="close"),
-                          custom.Button.inline("Next",
-                                               data="{}_next({})".format(
-                                                   prefix, modulo_page)),
-                      )]
+        pairs = pairs[
+            modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)
+        ] + [
+            (
+                custom.Button.inline(
+                    "Previous", data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                custom.Button.inline("❌Close", data="close"),
+                custom.Button.inline(
+                    "Next", data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
     return pairs
