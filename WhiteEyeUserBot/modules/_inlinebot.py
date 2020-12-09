b13bb29dbd6be4f42f297de944884720763f7d76
mrdayamzaidi <dayamali975@gmail.com>
2020-12-09 09:32:06 +0000

    pylint: auto fixes

--- WhiteEyeUserBot/modules/_inlinebot.py
+++ WhiteEyeUserBot/modules/_inlinebot.py
@@ -74,7 +74,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_popp_up_alert = "Please get your own WhiteEyeUserbot, and don't use mine!"
+            reply_popp_up_alert = (
+                "Please get your own WhiteEyeUserbot, and don't use mine!"
+            )
             await event.answer(reply_popp_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
