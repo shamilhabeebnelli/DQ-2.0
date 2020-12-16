c0951330318b990dd826903b10ea70418b794f1d
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-12-16 09:05:14 +0530

    Update pmpermit.py

--- WhiteEyeUserBot/modules/pmpermit.py
+++ WhiteEyeUserBot/modules/pmpermit.py
@@ -12,7 +12,7 @@ from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-    WARN_PIC = "https://telegra.ph/file/53aed76a90e38779161b1.jpg"
+    WARN_PIC = "https://telegra.ph/file/63d2f8bcdae4da2ec5e7e.jpg"
 else:
     WARN_PIC = PMPERMIT_PIC
 
@@ -25,7 +25,7 @@ DEFAULTUSER = (
     str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 )
 CUSTOM_MIDDLE_PMP = (
-    str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "Protection By WhiteEye 🇮🇳"
+    str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "Protection By WhiteEye🇮🇳"
 )
 USER_BOT_WARN_ZERO = "You Have Attempted To Spam Masters Inbox So Inorder To Avoid Over Spam , You Have Been Blocked By WhiteEye"
 
