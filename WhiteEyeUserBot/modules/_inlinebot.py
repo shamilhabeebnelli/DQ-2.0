82abdf6c7792900f577039cfde2a8fc1e0b103a6
mrdayamzaidi <dayamali975@gmail.com>
2020-12-19 09:34:59 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/modules/_inlinebot.py
+++ WhiteEyeUserBot/modules/_inlinebot.py
@@ -85,7 +85,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 "Menu Closed!!",
             )
         else:
-            reply_pop_up_alert = "Please get your own WhiteEyeuserbot from @WhiteEyeDevs "
+            reply_pop_up_alert = (
+                "Please get your own WhiteEyeuserbot from @WhiteEyeDevs "
+            )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
@@ -102,9 +104,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = (
-                "Please get your own WhiteEyeUserbot, and don't use mine Any Support Ask @WhiteEyeDevs!"
-            )
+            reply_pop_up_alert = "Please get your own WhiteEyeUserbot, and don't use mine Any Support Ask @WhiteEyeDevs!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
