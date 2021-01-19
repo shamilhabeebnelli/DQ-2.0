a9f8e9a3940476c787add057d900b332bb4c2bce
mrdayamzaidi <dayamali975@gmail.com>
2021-01-19 11:25:27 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/modules/meadmin.py
+++ WhiteEyeUserBot/modules/meadmin.py
@@ -3,17 +3,19 @@
 # © JAYANT KAGERI, ALL RIGHTS RESERVED
 
 
-from WhiteEyeUserBot.utils import WhiteEye_on_cmd 
-@WhiteEye.on(WhiteEye_on_cmd(pattern="meadmin", outgoing=True)) 
+from WhiteEyeUserBot.utils import WhiteEye_on_cmd
+
+
+@WhiteEye.on(WhiteEye_on_cmd(pattern="meadmin", outgoing=True))
 async def _1(event):
     addall = [
-            d.entity
-            for d in await event.client.get_dialogs()
-                if (d.is_group or d.is_channel)
-        ]
+        d.entity
+        for d in await event.client.get_dialogs()
+        if (d.is_group or d.is_channel)
+    ]
     for i in addall:
-      try:
-          if i.creator or i.admin_rights:
-                await bot.send_message(event.chat_id, i.title) 
-      except BaseException:
-          pass
+        try:
+            if i.creator or i.admin_rights:
+                await bot.send_message(event.chat_id, i.title)
+        except BaseException:
+            pass
