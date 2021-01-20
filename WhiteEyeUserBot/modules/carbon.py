850bdfa40e8280fb8a8e855b9781f2a3210ecbeb
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-20 23:05:04 +0530

    Update carbon.py

--- WhiteEyeUserBot/modules/carbon.py
+++ WhiteEyeUserBot/modules/carbon.py
@@ -94,7 +94,7 @@ async def carbon_api(e):
         await e.client.send_file(
             e.chat_id,
             file,
-            caption="<< `Here's your carbon!` \n **Carbonised Using** [Friday](https://github.com/Starkgang/FridayUserbot)>>\n**Colour Scheme: **`{}`".format(
+            caption="<< `Here's your carbon!` \n **Carbonised Using** [WhiteEye](https://t.me/whiteeyebots)>>\n**Colour Scheme: **`{}`".format(
                 color_name
             ),
             force_document=True,
