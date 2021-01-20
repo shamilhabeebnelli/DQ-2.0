d375ca90a5644bcb41476b8a1acbdce72bb710d8
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-20 15:59:07 +0530

    Update broadcast.py

--- WhiteEyeUserBot/modules/broadcast.py
+++ WhiteEyeUserBot/modules/broadcast.py
@@ -5,11 +5,11 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
 from WhiteEyeUserBot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from WhiteEyeUserBot.Configs import Config
+from WhiteEyeUserBot.Server import parse_pre
 from WhiteEyeUserBot.utils import (
     WhiteEye_on_cmd,
     edit_delete,
     edit_or_reply,
-    parse_pre,
     sudo_cmd,
 )
 
