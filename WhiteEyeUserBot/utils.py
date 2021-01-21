aa6e13890b0b74cdecc90ade456495b01a4cea21
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-21 13:04:05 +0530

    Update utils.py

--- WhiteEyeUserBot/utils.py
+++ WhiteEyeUserBot/utils.py
@@ -23,7 +23,6 @@ from pathlib import Path
 
 from telethon import events
 
-from var import Var
 from WhiteEyeUserBot import CMD_LIST, LOAD_PLUG, SUDO_LIST, bot
 from WhiteEyeUserBot.Configs import Config
 from WhiteEyeUserBot.wraptools import (
@@ -33,7 +32,7 @@ from WhiteEyeUserBot.wraptools import (
     ignore_grp,
     ignore_pm,
 )
-
+from var import Var
 sedprint = logging.getLogger("PLUGINS")
 cmdhandler = Config.COMMAND_HAND_LER
 bothandler = Config.BOT_HANDLER
@@ -123,7 +122,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        print("Successfully (re)imported " + shortname)
+        sedprint.info("Successfully (re)imported " + shortname)
     else:
         import importlib
         import sys
@@ -146,6 +145,8 @@ def load_module(shortname):
         sys.modules["WhiteEye.util"] = WhiteEyeUserBot.utils
         sys.modules["userbot.utils"] = WhiteEyeUserBot.utils
         sys.modules["userbot.plugins"] = WhiteEyeUserBot.modules
+        sys.modules["plugins"] = WhiteEyeUserBot.modules
+        sys.modules["userbot"] = WhiteEyeUserBot
         mod.Config = Config
         mod.ignore_grp = ignore_grp()
         mod.ignore_pm = ignore_pm()
@@ -159,7 +160,7 @@ def load_module(shortname):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["WhiteEyeUserBot.modules." + shortname] = mod
-        print("Successfully imported " + shortname)
+        sedprint.info("Successfully imported " + shortname)
 
 
 def remove_plugin(shortname):
@@ -341,7 +342,7 @@ def errors_handler(func):
 
             text = "**USERBOT CRASH REPORT**\n\n"
 
-            link = "[Here](https://t.me/WhiteEyeOT)"
+            link = "[Here](https://t.me/WhiteEyeDevs)"
             text += "If you wanna you can report it"
             text += f"- just forward this message {link}.\n"
             text += "Nothing is logged except the fact of error and date\n"
@@ -518,6 +519,8 @@ def assistant_cmd(add_cmd, is_args=False):
             pattern = bothandler + add_cmd + "(?: |$)(.*)"
         elif is_args == "stark":
             pattern = bothandler + add_cmd + " (.*)"
+        elif is_args == "heck":
+            pattern = bothandler + add_cmd
         elif is_args == "snips":
             pattern = bothandler + add_cmd + " (\S+)"
         else:
@@ -533,8 +536,8 @@ def is_admin():
     def decorator(func):
         @functools.wraps(func)
         async def wrapper(event):
-            bot.tgbot
-            sed = await arantxa.get_permissions(event.chat_id, event.sender_id)
+            serena = bot.tgbot
+            sed = await serena.get_permissions(event.chat_id, event.sender_id)
             user = event.sender_id
             kek = bot.uid
             if sed.is_admin:
@@ -555,9 +558,9 @@ def is_bot_admin():
     def decorator(func):
         @functools.wraps(func)
         async def wrapper(event):
-            bot.tgbot
-            pep = await arantxa.get_me()
-            sed = await arantxa.get_permissions(event.chat_id, pep)
+            serena = bot.tgbot
+            pep = await serena.get_me()
+            sed = await serena.get_permissions(event.chat_id, pep)
             if sed.is_admin:
                 await func(event)
             else:
@@ -674,8 +677,8 @@ def start_assistant(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        print("Starting Your Assistant Bot.")
-        print("Assistant Sucessfully imported " + shortname)
+        sedprint.info("Starting Your Assistant Bot.")
+        sedprint.info("Assistant Sucessfully imported " + shortname)
     else:
         import importlib
         import sys
@@ -686,7 +689,7 @@ def start_assistant(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.tgbot = bot.tgbot
-        mod.arantxa = bot.tgbot
+        mod.serena = bot.tgbot
         mod.assistant_cmd = assistant_cmd
         mod.god_only = god_only()
         mod.only_groups = only_groups()
@@ -699,91 +702,4 @@ def start_assistant(shortname):
         mod.only_pvt = only_pvt()
         spec.loader.exec_module(mod)
         sys.modules["WhiteEyeUserBot.modules.assistant" + shortname] = mod
-        print("Assistant Has imported " + shortname)
-
-
-async def edit_or_reply(
-    event,
-    text,
-    parse_mode=None,
-    link_preview=None,
-    file_name=None,
-    aslink=False,
-    linktext=None,
-    caption=None,
-):
-    link_preview = link_preview or False
-    reply_to = await event.get_reply_message()
-    if len(text) < 4096:
-        parse_mode = parse_mode or "md"
-        if event.sender_id in Config.SUDO_USERS:
-            if reply_to:
-                return await reply_to.reply(
-                    text, parse_mode=parse_mode, link_preview=link_preview
-                )
-            return await event.reply(
-                text, parse_mode=parse_mode, link_preview=link_preview
-            )
-        return await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
-    asciich = ["*", "`", "_"]
-    for i in asciich:
-        text = re.sub(rf"\{i}", "", text)
-    if aslink:
-        linktext = linktext or "Message was to big so pasted to bin"
-        try:
-            key = (
-                requests.post(
-                    "https://nekobin.com/api/documents", json={"content": text}
-                )
-                .json()
-                .get("result")
-                .get("key")
-            )
-            text = linktext + f" [here](https://nekobin.com/{key})"
-        except:
-            text = re.sub(r"•", ">>", text)
-            kresult = requests.post(
-                "https://del.dog/documents", data=text.encode("UTF-8")
-            ).json()
-            text = linktext + f" [here](https://del.dog/{kresult['key']})"
-        if event.sender_id in Config.SUDO_USERS:
-            if reply_to:
-                return await reply_to.reply(text, link_preview=link_preview)
-            return await event.reply(text, link_preview=link_preview)
-        return await event.edit(text, link_preview=link_preview)
-    file_name = file_name or "output.txt"
-    caption = caption or None
-    with open(file_name, "w+") as output:
-        output.write(text)
-    if reply_to:
-        await reply_to.reply(caption, file=file_name)
-        await event.delete()
-        return os.remove(file_name)
-    if event.sender_id in Config.SUDO_USERS:
-        await event.reply(caption, file=file_name)
-        await event.delete()
-        return os.remove(file_name)
-    await event.client.send_file(event.chat_id, file_name, caption=caption)
-    await event.delete()
-    os.remove(file_name)
-
-
-async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None):
-    parse_mode = parse_mode or "md"
-    link_preview = link_preview or False
-    time = time or 5
-    if event.sender_id in Config.SUDO_USERS:
-        reply_to = await event.get_reply_message()
-        catevent = (
-            await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
-            if reply_to
-            else await event.reply(
-                text, link_preview=link_preview, parse_mode=parse_mode
-            )
-        )
-    else:
-        catevent = await event.edit(
-            text, link_preview=link_preview, parse_mode=parse_mode
-        )
-    await asyncio.sleep(time)
-    return await catevent.delete()
+        sedprint.info("Assistant Has imported " + shortname)
