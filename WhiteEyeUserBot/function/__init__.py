26df5dd88c8c7e5b46ec8bad624319aa339a7aff
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-21 12:27:18 +0530

    Update __init__.py

--- WhiteEyeUserBot/function/__init__.py
+++ WhiteEyeUserBot/function/__init__.py
@@ -488,3 +488,71 @@ async def _ytdl(url, is_it, event, tgbot):
             file=hmmo, text=f"{ytdl_data['title']} \n**Uploaded Using @WhiteEyeDevs**"
         )
         os.remove(f"{ytdl_data['id']}.mp4")
+                  
+                  
+# Thanks To @HeisenbergTheDanger, @xditya
+async def fetch_feds(event, borg):
+    fedList = []
+    await event.edit("`Fetching Your FeD List`, This May Take A While.")
+    reply_s = await event.get_reply_message()
+    if reply_s and reply_s.media:
+        downloaded_file_name = await borg.download_media(reply_s.media, "fedlist.txt")
+        await asyncio.sleep(1)
+        file = open(downloaded_file_name, "r")
+        lines = file.readlines()
+        for line in lines:
+            try:
+                fedList.append(line[:36])
+            except:
+                pass
+                # CleanUp
+        os.remove(downloaded_file_name)
+        return fedList
+    async with borg.conversation("@MissRose_bot") as bot_conv:
+        await bot_conv.send_message("/start")
+        await bot_conv.send_message("/myfeds")
+        await asyncio.sleep(3)
+        response = await bot_conv.get_response(timeout=300)
+        await asyncio.sleep(3)
+        if "You can only use fed commands once every 5 minutes" in response.text:
+            await event.edit("`Try again after 5 mins.`")
+            return
+        elif "make a file" in response.text:
+            await event.edit(
+                "`Boss, You Real Peru. You Are Admin in So Many Feds. WoW!`"
+            )
+            await asyncio.sleep(2)
+            await response.click(0)
+            await asyncio.sleep(6)
+            fedfile = await bot_conv.get_response()
+            await asyncio.sleep(2)
+            if fedfile.media:
+                downloaded_file_name = await borg.download_media(fedfile, "fedlist.txt")
+                await asyncio.sleep(1)
+                file = open(downloaded_file_name, "r")
+                lines = file.readlines()
+                for line in lines:
+                    try:
+                        fedList.append(line[:36])
+                    except BaseException:
+                        pass
+                # CleanUp
+                os.remove(downloaded_file_name)
+        else:
+            In = False
+            tempFedId = ""
+            for x in response.text:
+                if x == "`":
+                    if In:
+                        In = False
+                        fedList.append(tempFedId)
+                        tempFedId = ""
+                    else:
+                        In = True
+
+                elif In:
+                    tempFedId += x
+    await event.edit("`FeD List Fetched SucessFully.`")
+    return fedList
+
+                  
