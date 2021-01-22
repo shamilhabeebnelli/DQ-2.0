22f79656f36c3a67eecd37526b04adc94f3f617d
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-22 12:32:32 +0530

    Add files via upload

---
+++ WhiteEyeUserBot/modules/news.py
@@ -0,0 +1,41 @@
+# Copyright (C) By Dayamzaidi [@mrdayamzaidi]
+# Don't edit credits
+# Works On Bases Of Cyberboysumanjay's Inshorts News Api
+# Test
+
+import requests
+
+from var import Var
+from WhiteEyeUserBot.utils import WhiteEye_on_cmd, edit_or_reply, sudo_cmd
+
+newslog = Var.NEWS_CHANNEL_ID
+
+
+@WhiteEye.on(WhiteEye_on_cmd("news (.*)"))
+@WhiteEye.on(sudo_cmd("news (.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    if Var.NEWS_CHANNEL_ID is None:
+        await edit_or_reply(
+            event, "`Please ADD NEWS_CHANNEL_ID For This Module To Work`"
+        )
+        return
+    infintyvar = event.pattern_match.group(1)
+    main_url = f"https://inshortsapi.vercel.app/news?category={infintyvar}"
+    stuber = await edit_or_reply(
+        event,
+        f"Ok ! Fectching {infintyvar} From inshortsapi Server And Sending To News Channel",
+    )
+    await stuber.edit("All News Has Been Sucessfully Send To News Channel")
+    starknews = requests.get(main_url).json()
+    for item in starknews["data"]:
+        sedlyf = item["content"]
+        img = item["imageUrl"]
+        writter = item["author"]
+        dateis = item["date"]
+        readthis = item["readMoreUrl"]
+        titles = item["title"]
+        sed1 = img
+        sedm = f"**Title : {titles}** \n{sedlyf} \nDate : {dateis} \nAuthor : {writter} \nReadMore : {readthis}"
+        await borg.send_file(newslog, sed1, caption=sedm)
