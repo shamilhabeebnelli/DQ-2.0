3c36b39476896d73904d442c728f12f843b2d420
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-30 15:01:52 +0530

    Update repeat.py

--- WhiteEyeUserBot/modules/repeat.py
+++ WhiteEyeUserBot/modules/repeat.py
@@ -5,6 +5,8 @@ from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 
 @WhiteEye.on(WhiteEye_on_cmd("repeat ?(.*)"))
 async def _(event):
+    if event.fwd_from:
+        return
     message = event.text[10:]
     count = int(event.text[8:10])
     repmessage = message * count
