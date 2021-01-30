c1f67b403a1f71eacdebeb857be53a13a28280c8
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-30 14:44:46 +0530

    Update meadmin.py

--- WhiteEyeUserBot/modules/meadmin.py
+++ WhiteEyeUserBot/modules/meadmin.py
@@ -8,6 +8,8 @@ from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 
 @WhiteEye.on(WhiteEye_on_cmd(pattern="meadmin", outgoing=True))
 async def _1(event):
+    if event.fwd_from:
+        return
     addall = [
         d.entity
         for d in await event.client.get_dialogs()
