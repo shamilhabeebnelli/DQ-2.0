60ebfd84fdacc0f1963b934188a3769b42c42570
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-30 14:39:10 +0530

    Update listmyusernames.py

--- WhiteEyeUserBot/modules/listmyusernames.py
+++ WhiteEyeUserBot/modules/listmyusernames.py
@@ -12,6 +12,8 @@ async def mine(event):
     """ For .reserved command, get a list of your reserved usernames. """
     result = await bot(GetAdminedPublicChannelsRequest())
     output_str = ""
+    if event.fwd_from:
+        return
     for channel_obj in result.chats:
         output_str += f"{channel_obj.title}\n@{channel_obj.username}\n\n"
     await event.edit(output_str)
