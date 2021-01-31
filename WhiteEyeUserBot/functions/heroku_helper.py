2c0022e7e1b77ae141ff5c1c39f5f89b434bf06e
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-31 18:33:55 +0530

    Update heroku_helper.py

--- WhiteEyeUserBot/functions/heroku_helper.py
+++ WhiteEyeUserBot/functions/heroku_helper.py
@@ -2,9 +2,9 @@
 
 import heroku3
 
-from var import Var
+from WhiteEyeUserBot.Configs import Config
 
-herokuclient = heroku3.from_key(Var.HEROKU_API_KEY)
+herokuclient = heroku3.from_key(Config.HEROKU_API_KEY)
 
 
 class HerokuHelper:
@@ -23,11 +23,11 @@ class HerokuHelper:
     def getLog(self):
         return self.app.get_log()
 
-    def addEnvVar(self, key, value):
+    def addEnvConfig(self, key, value):
         self.app.config()[key] = value
 
     def restart(self):
         return self.app.restart()
-
+    
     def shutdown(self):
         return self.app.process_formation()["worker.1"].scale(0)
