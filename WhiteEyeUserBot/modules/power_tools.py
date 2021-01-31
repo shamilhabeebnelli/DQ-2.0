8b622e1ce6dc0c8dde6325ed5665949571bdd657
mrdayamzaidi <dayamali975@gmail.com>
2021-01-31 13:48:17 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/modules/power_tools.py
+++ WhiteEyeUserBot/modules/power_tools.py
@@ -7,18 +7,23 @@ Available Commands:
 # file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html
 import os
 import sys
-from WhiteEyeUserBot.functions.heroku_helper import HerokuHelper
-from WhiteEyeUserBot import CMD_HELP
-from WhiteEyeUserBot.utils import WhiteEye_on_cmd
+
 import heroku3
-import requests
+
+from WhiteEyeUserBot import CMD_HELP
+from WhiteEyeUserBot.functions.heroku_helper import HerokuHelper
+from WhiteEyeUserBot.utils import WhiteEye_on_cmd
+
 Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
+
 
 @WhiteEye.on(WhiteEye_on_cmd("restart"))
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("**Restarting WhiteEye ! If You Want To Check If I am Alive, Do** `.ping` After 2 Min!")
+    await event.edit(
+        "**Restarting WhiteEye ! If You Want To Check If I am Alive, Do** `.ping` After 2 Min!"
+    )
     if Config.HEROKU_API_KEY:
         herokuHelper = HerokuHelper(Config.HEROKU_APP_NAME, Config.HEROKU_API_KEY)
         herokuHelper.restart()
@@ -34,7 +39,7 @@ async def _(event):
     await event.edit("Turning off WhiteEye ...Manually turn me on later")
     if Config.HEROKU_API_KEY:
         app = Heroku.app(Config.HEROKU_APP_NAME)
-        app.dynos['worker.1'].kill()
+        app.dynos["worker.1"].kill()
     else:
         await bot.disconnect()
         sys.exit(0)
