11a8a5879cf19901f8e5f31a6ea4b9b0a7a851c2
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-31 19:04:46 +0530

    Update inline_fun.py

--- WhiteEyeUserBot/modules/inline_fun.py
+++ WhiteEyeUserBot/modules/inline_fun.py
@@ -1,4 +1,4 @@
-from var import Var
+from var import Config
 from WhiteEyeUserBot import CMD_HELP
 from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 
@@ -7,7 +7,7 @@ from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 async def stats(event):
     if event.fwd_from:
         return
-    botusername = Var.TG_BOT_USER_NAME_BF_HER
+    botusername = Config.TG_BOT_USER_NAME_BF_HER
     noob = "stats"
     if event.reply_to_msg_id:
         await event.get_reply_message()
