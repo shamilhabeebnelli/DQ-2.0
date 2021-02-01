337ede25de9aabe1e99e2771b96665f74e603faf
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-02-01 14:53:03 +0530

    Update _inlinebot.py

--- WhiteEyeUserBot/modules/_inlinebot.py
+++ WhiteEyeUserBot/modules/_inlinebot.py
@@ -1,12 +1,22 @@
 import os
 import re
+import urllib
+import json
 from math import ceil
-
+from re import findall
+import requests
+from youtube_search import YoutubeSearch
+from search_engine_parser import GoogleSearch
+from WhiteEyeUserBot.functions import _ytdl, fetch_json, _deezer_dl, all_pro_s
+from urllib.parse import quote
+import requests
 from telethon import Button, custom, events, functions
-
-from WhiteEyeUserBot import ALIVE_NAME, CMD_HELP, CMD_LIST
+from youtubesearchpython import VideosSearch
+from WhiteEyeUserBot import ALIVE_NAME, CMD_HELP, CMD_LIST, client2 as client1, client3 as client2, bot as client3
 from WhiteEyeUserBot.modules import inlinestats
-
+from pornhub_api import PornhubApi
+from telethon.tl.types import BotInlineResult, InputBotInlineMessageMediaAuto, DocumentAttributeImageSize, InputWebDocument, InputBotInlineResult
+from telethon.tl.functions.messages import SetInlineBotResultsRequest
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
     WARN_PIC = "https://telegra.ph/file/63d2f8bcdae4da2ec5e7e.jpg"
@@ -14,41 +24,40 @@ else:
     WARN_PIC = PMPERMIT_PIC
 LOG_CHAT = Config.PRIVATE_GROUP_ID
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "WhiteEye"
-if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
-    @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
-    async def inline_handler(event):
-        builder = event.builder
-        result = None
-        query = event.text
-        if event.query.user_id == bot.uid and query.startswith("WhiteEye"):
-            rev_text = query[::-1]
-            buttons = paginate_help(0, CMD_LIST, "helpme")
-            result = builder.article(
-                "© WhiteEye Help",
-                text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),
-                buttons=buttons,
-                link_preview=False,
-            )
-        if event.query.user_id == bot.uid and query == "stats":
-            result = builder.article(
-                title="Stats",
-                text=f"**Showing Stats For {DEFAULTUSER}'s WhiteEye** \nNote --> Only Owner Can Check This \n(C) @Whiteeyeot",
-                buttons=[
-                    [custom.Button.inline("Show Stats ", data="terminator")],
-                    [
-                        Button.url(
-                            "Repo 🇮🇳", "https://github.com/WhiteEye-Org/WhiteEyeUserBot"
-                        )
-                    ],
-                    [Button.url("Join Channel ⚓", "t.me/WhiteEyeOT")],
-                ],
-            )
-        if event.query.user_id == bot.uid and query.startswith("**Hello"):
-            result = builder.photo(
-                file=WARN_PIC,
-                text=query,
-                buttons=[
+
+@tgbot.on(events.InlineQuery)
+async def inline_handler(event):
+    o = await all_pro_s(Config, client1, client2, client3)
+    builder = event.builder
+    result = None
+    query = event.text
+    if event.query.user_id in o and query.startswith("WhiteEye"):
+        rev_text = query[::-1]
+        buttons = paginate_help(0, CMD_HELP, "helpme")
+        result = builder.article(
+            "© WhiteEyeUserBot Help",
+            text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),
+            buttons=buttons,
+            link_preview=False,
+        )
+        await event.answer([result])
+    elif event.query.user_id in o and query == "stats":
+        result = builder.article(
+            title="Stats",
+            text=f"**Showing Stats For {DEFAULTUSER}'s WhiteEyeUserBot** \nNote --> Only Owner Can Check This \n(C) @WhiteEyeDevs",
+            buttons=[
+                [custom.Button.inline("Show Stats ?", data="terminator")],
+                [Button.url("Repo 🇮🇳", "https://github.com/WhiteEye-Org/WhiteEyeUserBot")],
+                [Button.url("Join Channel ❤️", "t.me/WhiteEyeDevs")],
+            ],
+        )
+        await event.answer([result])
+    elif event.query.user_id in o and query.startswith("**Hello"):
+        result = builder.photo(
+            file=WARN_PIC,
+            text=query,
+            buttons=[
                     [
                         custom.Button.inline("❌ Spamming", data="wannaspam"),
                         custom.Button.inline("📝 Chatting", data="casualbitching"),
@@ -59,98 +68,85 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     ],
                 ],
             )
-        await event.answer([result] if result else None)
-
-    @tgbot.on(
-        events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-            data=re.compile(b"helpme_next\((.+?)\)")
-        )
+        await event.answer([result]
+    
+@tgbot.on(
+    events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+        data=re.compile(b"helpme_next\((.+?)\)")
     )
-    async def on_plug_in_callback_query_handler(event):
-        if event.query.user_id == bot.uid:  # pylint:disable=E0602
-            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
-            buttons = paginate_help(current_page_number + 1, CMD_LIST, "helpme")
-            # https://t.me/TelethonChat/115200
-            await event.edit(buttons=buttons)
-        else:
-            reply_popp_up_alert = (
-                "Please get your own WhiteEyeUserbot, and don't use mine!"
-            )
-            await event.answer(reply_popp_up_alert, cache_time=0, alert=True)
+)
+async def on_plug_in_callback_query_handler(event):
+    o = await all_pro_s(Config, client1, client2, client3)
+    if event.query.user_id in o:
+        current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+        buttons = paginate_help(current_page_number + 1, CMD_HELP, "helpme")
+        # https://t.me/TelethonChat/115200
+        await event.edit(buttons=buttons)
+    else:
+        reply_popp_up_alert = "Please get your own WhiteEyeUserBot, and don't use mine!"
+        await event.answer(reply_popp_up_alert, cache_time=0, alert=True)
 
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
-    async def on_plug_in_callback_query_handler(event):
-        if event.query.user_id == bot.uid:
-            await event.edit(
-                "Menu Closed!!",
-            )
-        else:
-            reply_pop_up_alert = "Please get your own WhiteEyeuserbot from @WhiteEyeOT "
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
-    @tgbot.on(
-        events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-            data=re.compile(b"helpme_prev\((.+?)\)")
+@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
+async def on_plug_in_callback_query_handler(event):
+    if event.query.user_id == bot.uid:
+        await event.edit(
+            "Menu Closed!!",
         )
-    )
-    async def on_plug_in_callback_query_handler(event):
-        if event.query.user_id == bot.uid:  # pylint:disable=E0602
-            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
-            buttons = paginate_help(
-                current_page_number - 1, CMD_LIST, "helpme"  # pylint:disable=E0602
-            )
-            # https://t.me/TelethonChat/115200
-            await event.edit(buttons=buttons)
-        else:
-            reply_pop_up_alert = (
-                "Please get your own WhiteEyeUserbot, and don't use mine!"
-            )
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+    else:
+        reply_pop_up_alert = "Please get your own WhiteEyeuserbot from @WhiteEyeDevs "
+        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-    @tgbot.on(
-        events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-            data=re.compile(b"us_plugin_(.*)")
-        )
+@tgbot.on(
+    events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+        data=re.compile(b"helpme_prev\((.+?)\)")
     )
-    async def on_plug_in_callback_query_handler(event):
-        if event.query.user_id == bot.uid:
-            plugin_name = event.data_match.group(1).decode("UTF-8")
-            help_string = ""
-            help_string += f"Commands Available in {plugin_name} - \n"
-            try:
-                if plugin_name in CMD_HELP:
-                    for i in CMD_HELP[plugin_name]:
-                        help_string += i
-                    help_string += "\n"
-                else:
-                    for i in CMD_LIST[plugin_name]:
-                        help_string += i
-                        help_string += "\n"
-            except BaseException:
-                pass
-            if help_string == "":
-                reply_pop_up_alert = "{} has no detailed info.\nUse .help {}".format(
-                    plugin_name, plugin_name
-                )
-            else:
-                reply_pop_up_alert = help_string
-            reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
-                © WhiteEye".format(
-                plugin_name
-            )
-            if len(help_string) >= 140:
-                oops = "List too long!\nCheck your saved messages!"
-                await event.answer(oops, cache_time=0, alert=True)
-                help_string += "\n\nThis will be auto-deleted in 1 minute!"
-                if bot is not None and event.query.user_id == bot.uid:
-                    ok = await bot.send_message("me", help_string)
-                    await asyncio.sleep(60)
-                    await ok.delete()
-            else:
-                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-        else:
-            reply_pop_up_alert = "You Cannot Become Barbosa Here!"
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+)
+async def on_plug_in_callback_query_handler(event):
+    o = await all_pro_s(Config, client1, client2, client3)
+    if event.query.user_id in o:  # pylint:disable=E0602
+        current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+        buttons = paginate_help(
+            current_page_number - 1, CMD_HELP, "helpme"  # pylint:disable=E0602
+        )
+        # https://t.me/TelethonChat/115200
+        await event.edit(buttons=buttons)
+    else:
+        reply_pop_up_alert = "Please get your own WhiteEyeUserBot, and don't use mine!"
+        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+@tgbot.on(
+    events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+        data=re.compile(b"us_plugin_(.*)")
+    )
+)
+async def on_plug_in_callback_query_handler(event):
+    o = await all_pro_s(Config, client1, client2, client3)
+    if not event.query.user_id in o:
+        sedok = "Who The Fuck Are You? Get Your Own WhiteEyeUserBot."
+        await event.answer(sedok, cache_time=0, alert=True)
+        return
+    plugin_name, page_number = event.data_match.group(1).decode("UTF-8").split("|", 1)
+    if plugin_name in CMD_HELP:
+        help_string = f"**💡 PLUGIN NAME 💡 :** `{plugin_name}` \n{CMD_HELP[plugin_name]}"
+    reply_pop_up_alert = help_string
+    reply_pop_up_alert += "\n\n**(C) @WhiteEyeDevs** ".format(plugin_name)
+    if len(reply_pop_up_alert) >= 4096:
+        crackexy = "`Pasting Your Help Menu.`"
+        await event.answer(crackexy, cache_time=0, alert=True)
+        out_file = reply_pop_up_alert
+        url = "https://del.dog/documents"
+        r = requests.post(url, data=out_file.encode("UTF-8")).json()
+        url = f"https://del.dog/{r['key']}"
+        await event.edit(
+            f"Pasted {plugin_name} to {url}",
+            link_preview=False,
+            buttons=[[custom.Button.inline("Go Back", data=f"backme_{page_number}")]],
+        )
+    else:
+        await event.edit(
+            message=reply_pop_up_alert,
+            buttons=[[custom.Button.inline("Go Back", data=f"backme_{page_number}")]],
+        )
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"terminator")))
     async def rip(event):
@@ -161,41 +157,67 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             txt = "You Can't View My Masters Stats"
             await event.answer(txt, alert=True)
 
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"wannaspam")))
-    async def rip(event):
-        await event.get_chat()
-        text1 = "You Have Chosed A Probhited Option. Therefore, You Have Been Blocked By WhiteEye. 💢"
-        await event.edit("Choice Not Accepted ❌")
-        await borg.send_message(event.query.user_id, text1)
-        await borg(functions.contacts.BlockRequest(event.query.user_id))
+@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"wannaspam")))
+async def rip(event):
+    o = await all_pro_s(Config, client1, client2, client3)
+    if event.query.user_id in o:
+        sedok = "Master, You Don't Need To Use This."
+        await event.answer(sedok, cache_time=0, alert=True)
+        return
+    await event.get_chat()
+    him_id = event.query.user_id
+    text1 = "**You Have Chosed A Probhited Option. Therefore, You Have Been Blocked By WhiteEye. 💢**"
+    await event.edit(text1)
+    await borg(functions.contacts.BlockRequest(event.query.user_id))
+    PM_E = f"**#PMEVENT** \nUser ID : {him_id} \n**This User Choose Probhited Option, So Has Been Blocked !** \n[Contact Him](tg://user?id={him_id})"
+    await borg.send_message(
+        LOG_CHAT,
+        message=PM_E)
 
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"casualbitching")))
-    async def rip(event):
-        await event.get_chat()
-        him_id = event.query.user_id
-        await event.edit("Choice Accepted ✔️")
-        text2 = "Ok. Please Wait Until My Master Approves. Don't Spam Or Try Anything Stupid. \nThank You For Contacting Me."
-        await borg.send_message(event.query.user_id, text2)
-        await tgbot.send_message(
-            LOG_CHAT,
-            message=f"Hello, A [New User](tg://user?id={him_id}). Wants To Talk With You.",
-            buttons=[Button.url("Contact Him", f"tg://user?id={him_id}")],
-        )
+@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"casualbitching")))
+async def rip(event):
+    o = await all_pro_s(Config, client1, client2, client3)
+    if event.query.user_id in o:
+        sedok = "Master, You Don't Need To Use This."
+        await event.answer(sedok, cache_time=0, alert=True)
+        return
+    await event.get_chat()
+    him_id = event.query.user_id
+    await event.edit("Ok. Please Wait Until My Master Approves. Don't Spam Or Try Anything Stupid. \nThank You For Contacting Me.")
+    PM_E = f"**#PMEVENT** \nUser ID : {him_id} \n**This User Wanted To Talk To You.** \n[Contact Him](tg://user?id={him_id})"
+    await borg.send_message(
+        LOG_CHAT,
+        message=PM_E)
 
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"askme")))
-    async def rip(event):
-        await event.get_chat()
-        await event.edit("Choice Accepted ✔️")
-        text3 = "Ok, Wait. You can Ask After Master Approves You. Kindly, Wait."
-        await borg.send_message(event.query.user_id, text3)
+@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"askme")))
+async def rip(event):
+    o = await all_pro_s(Config, client1, client2, client3)
+    if event.query.user_id in o:
+        sedok = "Master, You Don't Need To Use This."
+        await event.answer(sedok, cache_time=0, alert=True)
+        return
+    await event.get_chat()
+    him_id = event.query.user_id
+    await event.edit("Ok, Wait. You can Ask After Master Approves You. Kindly, Wait.")
+    PM_E = f"**#PMEVENT** \nUser ID : {him_id} \n**This User Wanted To Ask You Something** \n[Contact Him](tg://user?id={him_id})"
+    await borg.send_message(
+        LOG_CHAT,
+        message=PM_E)
 
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"others")))
-    async def rip(event):
-        await event.get_chat()
-        await event.edit("Choice Accepted ✔️")
-        text4 = "Ok, Wait. You can Ask After Master Approves You. Kindly, Wait."
-        await borg.send_message(event.query.user_id, text4)
-
+@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"others")))
+async def rip(event):
+    o = await all_pro_s(Config, client1, client2, client3)
+    if event.query.user_id in o:
+        sedok = "Master, You Don't Need To Use This."
+        await event.answer(sedok, cache_time=0, alert=True)
+        return
+    await event.get_chat()
+    him_id = event.query.user_id
+    await event.edit("Ok, Wait. You can Ask After Master Approves You. Kindly, Wait.")
+    PM_E = f"**#PMEVENT** \nUser ID : {him_id} \n**This User Wanted To Talk To You.** \n[Contact Him](tg://user?id={him_id})"
+    await borg.send_message(
+        LOG_CHAT,
+        message=PM_E)
 
 def paginate_help(page_number, loaded_modules, prefix):
     number_of_rows = 10
@@ -231,3 +253,18 @@ def paginate_help(page_number, loaded_modules, prefix):
             )
         ]
     return pairs
+                     
+tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"backme_(.*)")))
+async def sed(event):
+    sedm = int(event.data_match.group(1).decode("UTF-8"))
+    o = await all_pro_s(Config, client1, client2, client3)
+    if event.query.user_id not in o:
+        sedok = "Who The Fuck Are You? Get Your Own Friday."
+        await event.answer(sedok, cache_time=0, alert=True)
+        return
+    await event.answer("Back", cache_time=0, alert=False)
+    # This Is Copy of Above Code. (C) @SpEcHiDe
+    buttons = paginate_help(sedm, CMD_HELP, "helpme")
+    sed = f"""WhiteEyeUserBot Userbot Modules Are Listed Here !\n
+For More Help or Support Visit @WhiteEyeDevs \nCurrently Loaded Plugins: {len(CMD_LIST)}"""
+    await event.edit(message=sed, buttons=buttons)                           
