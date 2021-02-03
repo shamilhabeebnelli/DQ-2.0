2e52443f9fdfb8e70607283d04b453b3d0e3179f
mrdayamzaidi <dayamali975@gmail.com>
2021-02-03 16:54:05 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/modules/server_pinger.py
+++ WhiteEyeUserBot/modules/server_pinger.py
@@ -1,10 +1,12 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import requests
-from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from apscheduler.executors.pool import ProcessPoolExecutor, ThreadPoolExecutor
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 from WhiteEyeUserBot.modules.sql_helper import server_pinger_sql as whiteeyeping
 from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 
 if Config.PING_SERVERS:
+
     @WhiteEye.on(WhiteEye_on_cmd(pattern="aping"))
     async def _(event):
         if event.fwd_from:
@@ -16,6 +18,7 @@ if Config.PING_SERVERS:
             return
         whiteeyeping.add_ping(url)
         await event.edit(f"**URL :** `{url}` **Sucessfully Added To Db**")
+
     @WhiteEye.on(WhiteEye_on_cmd(pattern="rping"))
     async def _(event):
         if event.fwd_from:
@@ -27,28 +30,27 @@ if Config.PING_SERVERS:
             return
         whiteeyeping.rmping(url)
         await event.edit(f"**URL :** `{url}` **Sucessfully Removed From DataBase**")
-    
+
     async def ping_servers():
         hmm_p = 0
         url_s = whiteeyeping.get_all_url()
-        header_s = {"User-Agent": 'Server Pinged By @WhiteEyeDevs'}
+        header_s = {"User-Agent": "Server Pinged By @WhiteEyeDevs"}
         if len(url_s) == 0:
             return
         for i in url_s:
             try:
-              ws = requests.get(url=i.url, headers=header_s).status_code
-              logger.info(f"Pinged {i.url} // Status Code Recived : {ws}")
+                ws = requests.get(url=i.url, headers=header_s).status_code
+                logger.info(f"Pinged {i.url} // Status Code Recived : {ws}")
             except:
-              hmm_p += 1
+                hmm_p += 1
         success_l = len(url_s) - hmm_p
         logger.info(f"Sucessfully Pinged {success_l} Urls Out Of {len(url_s)}")
-    
-    
+
     scheduler = AsyncIOScheduler(
         executors={
-        'threadpool': ThreadPoolExecutor(max_workers=9),
-        'processpool': ProcessPoolExecutor(max_workers=3)
+            "threadpool": ThreadPoolExecutor(max_workers=9),
+            "processpool": ProcessPoolExecutor(max_workers=3),
         }
     )
-    scheduler.add_job(ping_servers, 'interval', minutes=60, executor='threadpool')
+    scheduler.add_job(ping_servers, "interval", minutes=60, executor="threadpool")
     scheduler.start()
