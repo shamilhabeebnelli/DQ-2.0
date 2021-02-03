f4134815f69397dc2f55b63fbfc8c05ca6ebe6c4
mrdayamzaidi <dayamali975@gmail.com>
2021-02-03 17:01:53 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/modules/amazon_white_tracker.py
+++ WhiteEyeUserBot/modules/amazon_white_tracker.py
@@ -1,23 +1,33 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from WhiteEyeUserBot.modules.sql_helper.amazon_tracker_sql import add_new_tracker, is_tracker_in_db, rm_tracker, get_tracker_info, rm_tracker, get_all_tracker, get_all_urls
 import requests
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from bs4 import BeautifulSoup
+
+from WhiteEyeUserBot.modules.sql_helper.amazon_tracker_sql import (
+    add_new_tracker,
+    get_all_tracker,
+    get_all_urls,
+    is_tracker_in_db,
+    rm_tracker,
+)
 from WhiteEyeUserBot.utils import WhiteEye_on_cmd
+
 
 @WhiteEye.on(WhiteEye_on_cmd(pattern="amt"))
 async def _(event):
     if event.fwd_from:
         return
     await event.edit("`WhiteEye Processing..`")
-    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+    }
     url = event.text.split(" ", maxsplit=1)[1]
     try:
-        page = requests.get(url, headers = headers)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        title = soup.find(id = "productTitle").get_text()
-        price = soup.find(id = "priceblock_ourprice").get_text()
+        page = requests.get(url, headers=headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+        title = soup.find(id="productTitle").get_text()
+        price = soup.find(id="priceblock_ourprice").get_text()
         title = title.strip()
-        price = price[2:].split(',')
+        price = price[2:].split(",")
         price = round(float("".join(price)))
     except:
         await event.edit("**Invalid Url !**")
@@ -26,8 +36,11 @@ async def _(event):
         await event.edit("**Tracker Already Found In Db**")
         return
     add_new_tracker(url, price)
-    await event.edit(f"Product Name : {title} \nCurrent Price : {price} \n**Added To TrackerList**")
-    
+    await event.edit(
+        f"Product Name : {title} \nCurrent Price : {price} \n**Added To TrackerList**"
+    )
+
+
 @WhiteEye.on(WhiteEye_on_cmd(pattern="rmt"))
 async def _(event):
     if event.fwd_from:
@@ -47,27 +60,34 @@ async def _(event):
         return
     rm_tracker(str(url))
     await event.edit(f"**Sucessfully Removed From TrackerList**")
-    
+
+
 async def track_amazon():
-    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
-    ws = get_all_urls() 
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+    }
+    ws = get_all_urls()
     if len(ws) == 0:
         return
     kk = get_all_tracker()
     for url in kk.amazon_url:
         for pm in kk.budget:
-            page = requests.get(url, headers = headers)
-            soup = BeautifulSoup(page.content, 'html.parser')
-            title = soup.find(id = "productTitle").get_text()
-            price = soup.find(id = "priceblock_ourprice").get_text()
+            page = requests.get(url, headers=headers)
+            soup = BeautifulSoup(page.content, "html.parser")
+            title = soup.find(id="productTitle").get_text()
+            price = soup.find(id="priceblock_ourprice").get_text()
             title = title.strip()
-            price = price[2:].split(',')
+            price = price[2:].split(",")
             price = round(float("".join(price)))
-            if (price <= pm):
-                await borg.send_message(Config.PRIVATE_GROUP_ID, f"#Tracker - Price Reduced \nProduct Name : {title} \nCurrent price : {price}")
+            if price <= pm:
+                await borg.send_message(
+                    Config.PRIVATE_GROUP_ID,
+                    f"#Tracker - Price Reduced \nProduct Name : {title} \nCurrent price : {price}",
+                )
                 rm_tracker(str(url))
             else:
                 pass
+
 
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
 scheduler.add_job(track_amazon, trigger="cron", hour=18, minute=40)
