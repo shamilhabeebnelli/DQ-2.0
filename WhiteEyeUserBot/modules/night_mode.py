54472dc64e4369ff73dd44ae334161c5c4f0b25f
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-02-05 11:07:32 +0530

    Update night_mode.py

--- WhiteEyeUserBot/modules/night_mode.py
+++ WhiteEyeUserBot/modules/night_mode.py
@@ -1,9 +1,8 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler 
 from telethon import functions
+from WhiteEyeUserBot.functions import get_all_admin_chats, is_admin
 from telethon.tl.types import ChatBannedRights
 
-from WhiteEyeUserBot.functions import is_admin
-from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 
 hehes = ChatBannedRights(
     until_date=None,
@@ -31,8 +30,6 @@ openhehe = ChatBannedRights(
     pin_messages=True,
     change_info=True,
 )
-
-
 @WhiteEye.on(WhiteEye_on_cmd(pattern="scgrp$"))
 async def close_ws(event):
     if not event.is_group:
@@ -42,17 +39,14 @@ async def close_ws(event):
         from WhiteEyeUserBot.modules.sql_helper import night_mode_sql as ws
     except:
         logger.info("Hehe, Kanger")
-    if not await is_admin(event, bot.uid):
+    if not await is_admin(event, bot.uid): 
         await event.edit("`You Should Be Admin To Do This!`")
         return
     if ws.is_nightmode_indb(event.chat_id):
         await event.edit("This Chat is Has Already Enabled Night Mode.")
         return
     ws.add_nightmode(event.chat_id)
-    await event.edit(
-        f"**Added Chat {event.chat.title} With Id {event.chat_id} To Database. This Group Will Be Closed On 12Am(IST) And Will Opened On 06Am(IST)**"
-    )
-
+    await event.edit(f"**Added Chat {event.chat.title} With Id {event.chat_id} To Database. This Group Will Be Closed On 12Am(IST) And Will Opened On 06Am(IST)**")
 
 @WhiteEye.on(WhiteEye_on_cmd(pattern="rsgrp$"))
 async def disable_ws(event):
@@ -63,16 +57,14 @@ async def disable_ws(event):
         from WhiteEyeUserBot.modules.sql_helper import night_mode_sql as ws
     except:
         logger.info("Hehe, Kanger")
-    if not await is_admin(event, bot.uid):
+    if not await is_admin(event, bot.uid): 
         await event.edit("`You Should Be Admin To Do This!`")
         return
     if not ws.is_nightmode_indb(event.chat_id):
         await event.edit("This Chat is Has Not Enabled Night Mode.")
         return
     ws.rmnightmode(event.chat_id)
-    await event.edit(
-        f"**Removed Chat {event.chat.title} With Id {event.chat_id} From Database. This Group Will Be No Longer Closed On 12Am(IST) And Will Opened On 06Am(IST)**"
-    )
+    await event.edit(f"**Removed Chat {event.chat.title} With Id {event.chat_id} From Database. This Group Will Be No Longer Closed On 12Am(IST) And Will Opened On 06Am(IST)**")
 
 
 async def job_close():
@@ -81,31 +73,27 @@ async def job_close():
     except:
         logger.info("Hehe, Kanger")
     ws_chats = ws.get_all_chat_id()
-    if len(ws_chats.chat_id) == 0:
+    if len(ws_chats) == 0:
         return
-    for warner in ws_chats.chat_id:
+    for warner in ws_chats:
         try:
             await WhiteEye.send_message(
-                warner,
-                "`12:00 Am, Group Is Closing Till 6 Am. Night Mode Started !` \n**Powered By @WhiteEyeDevs**",
+              warner, "`12:00 Am, Group Is Closing Till 6 Am. Night Mode Started !` \n**Powered By @WhiteEyeDevs**"
             )
             await WhiteEye(
-                functions.messages.EditChatDefaultBannedRightsRequest(
-                    peer=warner, banned_rights=hehes
-                )
+            functions.messages.EditChatDefaultBannedRightsRequest(
+                peer=warner, banned_rights=hehes
+            )
             )
             if Config.CLEAN_GROUPS:
                 async for user in WhiteEye.iter_participants(warner):
                     if user.deleted:
-                        await WhiteEye.edit_permissions(
-                            warner, user.id, view_messages=False
-                        )
+                        await friday.edit_permissions(warner, user.id, view_messages=False)
         except:
             pass
 
-
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
-scheduler.add_job(job_close, trigger="cron", hour=10, minute=15)
+scheduler.add_job(job_close, trigger="cron", hour=10, minute=35)
 scheduler.start()
 
 
@@ -115,23 +103,22 @@ async def job_open():
     except:
         logger.info("Hehe, Kanger")
     ws_chats = ws.get_all_chat_id()
-    if len(ws_chats.chat_id) == 0:
+    if len(ws_chats) == 0:
         return
-    for warner in ws_chats.chat_id:
+    for warner in ws_chats:
         try:
             await WhiteEye.send_message(
-                warner, "`06:00 Am, Group Is Opening.`\n**Powered By @WhiteEyeDevs**"
+              warner, "`06:00 Am, Group Is Opening.`\n**Powered By @WhiteEyeDevs**"
             )
             await WhiteEye(
-                functions.messages.EditChatDefaultBannedRightsRequest(
-                    peer=warner, banned_rights=openhehe
-                )
+            functions.messages.EditChatDefaultBannedRightsRequest(
+                peer=warner, banned_rights=openhehe
             )
+        )
         except:
             pass
 
-
 # Run everyday at 06
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
-scheduler.add_job(job_open, trigger="cron", hour=10, minute=20)
+scheduler.add_job(job_open, trigger="cron", hour=10, minute=40)
 scheduler.start()
